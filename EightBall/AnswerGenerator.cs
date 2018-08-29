3dc6409a02c283b97f31449070e8660d2747aabe
Van Tian <vantian@live.com>
2018-08-29 15:01:13 +0800

    XAML

---
+++ EightBall/AnswerGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EightBall
+{
+    internal class AnswerGenerator
+    {
+        public AnswerGenerator()
+        {
+        }
+
+        internal string GetRandomAnswer(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- EightBall/MainWindow.xaml.cs
+++ EightBall/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EightBall
 {
@@ -23,6 +13,17 @@ namespace EightBall
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void cmdAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            Cursor = Cursors.Wait;
+
+            Task.Delay(TimeSpan.FromSeconds(3)).GetAwaiter().GetResult();
+
+            var generator = new AnswerGenerator();
+            txtAnswer.Text = generator.GetRandomAnswer(txtQuestion.Text);
+            Cursor = null;
         }
     }
 }
